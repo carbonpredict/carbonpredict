038c10c34225192125e929960f9114a734bfe41d
Jyrki Martti <jmartti@users.noreply.github.com>
2020-06-22 15:52:44 +0300

    Removed some unnecessary code lines and added guidlines

--- restapi/dummyapi.py
+++ restapi/dummyapi.py
@@ -1,3 +1,6 @@
+# Run with python dummyapi.py
+# Then try e.g. http://127.0.0.1:5000/cloth/t-shirt on local computer
+
 from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
 import random
@@ -5,21 +8,9 @@ import random
 app = Flask(__name__)
 api = Api(app)
 
-co2e = random.randint(150,666)
-ci = round(co2e/10)
-mean = co2e - 2
-median = co2e - 4
 
-CO2E = {
-    'CO2e': co2e,
-    'confidence level': ci,
-    'mean': mean,
-    'median': median
-}
-
-
-# TodoList
-# shows a list of all todos, and lets you POST to add new tasks
+# Gives a random "prediction" for any cloth
+# E.g. http://127.0.0.1:5000/cloth/t-shirt on local computer
 class Predict(Resource):
     def get(self, cloth):
         co2e = random.randint(150,666)
@@ -34,9 +25,8 @@ class Predict(Resource):
             }
         return CO2E
 
-
 ##
-## Actually setup the Api resource routing here
+## Seteup the Api resource routing
 ##
 api.add_resource(Predict, '/cloth/<string:cloth>')
 
